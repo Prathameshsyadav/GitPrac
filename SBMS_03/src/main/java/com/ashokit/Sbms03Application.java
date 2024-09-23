a21df9e6dd31ea7e8955fa8aa2146f54cb94122a
Prathamesh Yadav <75838845+Prathameshsyadav@users.noreply.github.com>
2024-09-23 23:01:55 +0530

    Update Sbms03Application.java

--- SBMS_03/src/main/java/com/ashokit/Sbms03Application.java
+++ SBMS_03/src/main/java/com/ashokit/Sbms03Application.java
@@ -13,8 +13,8 @@ public class Sbms03Application {
 		ConfigurableApplicationContext config = SpringApplication.run(Sbms03Application.class, args);
 
 		Boolean b = true;
-
-		String s = "new file added"
+		int j = 200;
+		String s = "new file added";
 
 		UserServices bean = config.getBean(UserServices.class);
 		
